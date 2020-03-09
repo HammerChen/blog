ff9bb1150f0c865a988c3ce11e0883cc32f5d653
HammerChen <chenxiaochui@126.com>
2020-03-09 14:37:49 +0800

    添加环境变量 & 为生产环境编译做准备

--- server/index.js
+++ server/index.js
@@ -9,9 +9,14 @@ app.set('secret', process.env.SECRET)
 app.use(require('cors')())
 app.use(express.json())
 
+app.use('/admin', express.static(__dirname + '/admin'))
+app.use('/', express.static(__dirname + '/web'))
+
 require('./plugins/db')(app)
 require('./routes/admin')(app)
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000')
+const PORT = process.env.ADMIN_PORT
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
 })
